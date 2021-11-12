cbe276095e2758245248631878f8cc8e9f73ff19
孙振宇 <sunzhenyu@live.com>
2021-11-12 11:47:25 +0800

    add polyfill config

--- components.d.ts
+++ components.d.ts
@@ -5,6 +5,7 @@
 declare module 'vue' {
   export interface GlobalComponents {
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
+    VarAppBar: typeof import('@varlet/ui')['_AppBarComponent']
   }
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import * as path from 'path'
 import viteCompression from 'vite-plugin-compression'
 import components from 'unplugin-vue-components/vite'
 import { VarletUIResolver } from 'unplugin-vue-components/resolvers'
+import legacy from '@vitejs/plugin-legacy'
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
@@ -12,7 +14,10 @@ export default defineConfig({
         components({
             resolvers: [VarletUIResolver()],
             dts: true,
-        })
+        }),
+        legacy({
+            targets: ["> 1%", "last 4 versions", "not dead"]
+        }),
     ],
     resolve: {
         alias: {
